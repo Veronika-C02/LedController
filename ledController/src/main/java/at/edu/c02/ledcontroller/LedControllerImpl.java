ae4ee14dfae9363dcc2d2534d170e74a94a1e16e
vGuertl <veronika.guertl@gmail.com>
2024-01-27 10:55:50 +0100

    closes #5 status Funktion für Main implementiert. liefert status von einer LED zurück

--- ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
@@ -3,7 +3,9 @@ package at.edu.c02.ledcontroller;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.sound.midi.Soundbank;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * This class handles the actual logic
@@ -28,6 +30,32 @@ public class LedControllerImpl implements LedController {
         // read int and string properties of the light
         System.out.println("First light id is: " + firstLight.getInt("id"));
         System.out.println("First light color is: " + firstLight.getString("color"));
+    }
+
+    @Override
+    public void status() throws IOException {
+
+        System.out.println("Geben Sie hier die ID des LEDs an: ");
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        int inputID = Integer.parseInt(inputString);
+
+        // Call `getLights`, the response is a json object in the form `{ "lights": [ { ... }, { ... } ] }`
+        JSONObject response = apiService.getLights(inputID);
+        // get the "lights" array from the response
+        JSONArray lights = response.getJSONArray("lights");
+        // read the first json object of the lights array
+        JSONObject specifiedLed = lights.getJSONObject(0);
+        // read int and string properties of the light
+
+        String onOffButton;
+        if (specifiedLed.getBoolean("on") == true){
+            onOffButton = "on";
+        }else onOffButton = "off";
+
+        System.out.println("LED: "+ inputID + " is currently " + onOffButton + ". Color: " + specifiedLed.getString("color"));
+
+
     }
 
 
